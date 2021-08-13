6e0f1276a9fcfedef88a476fd370baa5ace5cccb
Sergio Felipe Rezende do Nascimento <sergiorezende2001@gmail.com>
2021-08-13 20:46:39 -0300

    Adição dos parâmetros de property.

--- Código/src/main/java/br/ufrrj/quicksell/entities/Property.java
+++ Código/src/main/java/br/ufrrj/quicksell/entities/Property.java
@@ -1,7 +1,15 @@
 package br.ufrrj.quicksell.entities;
 
+import br.ufrrj.quicksell.utils.Adress;
+import br.ufrrj.quicksell.utils.Date;
+
 public class Property {
     int area;
-    int valor;
-    String adress;
+    int value;
+    String description;
+    Adress adress;
+    Date registerDate;
+
+    public Property (){
+    }
 }
