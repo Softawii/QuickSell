b1666cfd0a9908271acf249f4dee510d8de281ff
Sergio Felipe Rezende do Nascimento <sergiorezende2001@gmail.com>
2021-08-16 23:06:54 -0300

    Adicionando lista de imagens na propriedade

--- Código/src/main/java/br/ufrrj/quicksell/entities/Property.java
+++ Código/src/main/java/br/ufrrj/quicksell/entities/Property.java
@@ -3,12 +3,16 @@ package br.ufrrj.quicksell.entities;
 import br.ufrrj.quicksell.utils.Adress;
 import br.ufrrj.quicksell.utils.Date;
 
+import javax.swing.*;
+import java.util.List;
+
 public class Property {
     int area;
     int value;
     String description;
     Adress adress;
     Date registerDate;
+    List<ImageIcon> images;
 
     public Property(int area, int value, String description, Adress adress, Date registerDate) {
         this.area = area;
