80cc184bbdad04a5921ba7212d977ee26fbd771b
Sergio Felipe Rezende do Nascimento <sergiorezende2001@gmail.com>
2021-08-16 23:06:41 -0300

    Adição do PropertyFrame
    Frame que vai mostrar mais informações sobre o imóvel selecionado

--- Código/src/main/java/br/ufrrj/quicksell/Main.java
+++ Código/src/main/java/br/ufrrj/quicksell/Main.java
@@ -1,7 +1,11 @@
 package br.ufrrj.quicksell;
 
+import br.ufrrj.quicksell.entities.Property;
+import br.ufrrj.quicksell.utils.Adress;
+import br.ufrrj.quicksell.utils.Date;
 import br.ufrrj.quicksell.views.HomeFrame;
 import br.ufrrj.quicksell.views.LoginFrame;
+import br.ufrrj.quicksell.views.PropertyFrame;
 import com.formdev.flatlaf.intellijthemes.FlatCyanLightIJTheme;
 import com.formdev.flatlaf.intellijthemes.FlatDraculaIJTheme;
 import com.formdev.flatlaf.intellijthemes.FlatOneDarkIJTheme;
@@ -16,5 +20,11 @@ public class Main {
 
         new HomeFrame(1024, 720);
         new LoginFrame(400, 500);
+
+        Adress adress = new Adress("Soares", "26379060", "Vila Sao Joao", "40", "Queimados", "Rio de Janeiro", " ");
+        Property property = new Property(250, 250000, "Totalmente mobilada e excelente bairro. Realmente uma das casas ja vistas, mt braba msm",
+               adress, new Date(20,02,2001));
+
+        new PropertyFrame(property);
     }
 }
---
+++ Código/src/main/java/br/ufrrj/quicksell/views/PropertyFrame.java
@@ -0,0 +1,56 @@
+package br.ufrrj.quicksell.views;
+
+import br.ufrrj.quicksell.entities.Property;
+import br.ufrrj.quicksell.views.panels.MenuPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.util.Objects.requireNonNull;
+
+public class PropertyFrame extends JFrame {
+    private static final int WIDTH = 1024;
+    private static final int HEIGHT = 720;
+
+    private JPanel west;
+
+    private JPanel center;
+    private JPanel centerHeader;
+    private JPanel centerBody;
+    private JPanel centerFooter;
+
+    public PropertyFrame(Property property) {
+        super();
+        ImageIcon image = new ImageIcon(requireNonNull(this.getClass().getClassLoader().getResource("LoginIcon.png")));
+        this.setIconImage(image.getImage());
+        this.setTitle("Quicksell"); //Bota t�tulo
+        this.setSize(WIDTH, HEIGHT); //Muda o tamanho
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Fechar a aplica��o ao clicar no X
+        this.setResizable(true); //Permite que o usu�rio mude o tamanho da janela
+        this.setLocationRelativeTo(null);
+        this.setLayout(new BorderLayout(0,5));
+
+        west = new MenuPanel();
+        this.add(west, BorderLayout.WEST);
+
+        createCenter();
+
+        this.setVisible(true);
+    }
+
+    public void createCenter() {
+        center = new JPanel();
+        center.setLayout(new BorderLayout());
+        {
+            centerHeader = new JPanel();
+            center.add(centerHeader, BorderLayout.NORTH);
+
+            centerBody = new JPanel();
+            center.add(centerBody, BorderLayout.CENTER);
+
+            centerFooter = new JPanel();
+            center.add(centerFooter, BorderLayout.SOUTH);
+        }
+        this.add(center, BorderLayout.CENTER);
+    }
+}
